b6469ceb57eb4bd84f60b84f1b432c4c9b8d555a
Steven.Yin <yinzifancn@gmail.com>
2021-07-24 01:45:27 +0800

    mybatis with kotlin test passed

--- crowdfunding00-parent/crowdfunding01-webui/src/test/kotlin/cc/stevenyin/crowd/CrowdTest.kt
+++ crowdfunding00-parent/crowdfunding01-webui/src/test/kotlin/cc/stevenyin/crowd/CrowdTest.kt
@@ -1,29 +1,33 @@
 package cc.stevenyin.crowd
 
-import org.junit.runner.RunWith
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
-import org.springframework.test.context.ContextConfiguration
-import javax.sql.DataSource
-import org.junit.Test
-import org.springframework.beans.factory.annotation.Autowired
+import cc.stevenyin.crowd.entity.TestRecord
 import cc.stevenyin.crowd.mapper.TestMapper
+import cc.stevenyin.crowd.mapper.insert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import javax.sql.DataSource
 
 @RunWith(SpringJUnit4ClassRunner::class)
-@ContextConfiguration(locations = arrayOf("classpath:spring-persist-mybatis.xml"))
+@ContextConfiguration(locations = ["classpath:spring-persist-mybatis.xml"])
 class CrowdTest {
-	
-	@Autowired
-	private lateinit var dataSource: DataSource
+
+    @Autowired
+    private lateinit var dataSource: DataSource
+
     @Autowired
     private lateinit var testMapper: TestMapper
-	
-	@Test
-	fun testConnection() {
-		println(dataSource);
-		val test = cc.stevenyin.crowd.entity.TestRecord();
-		test.id = "123"
-		test.age = 1
-		test.description = "test"
-		test.name = "steven"
-	}
+
+    @Test
+    fun testConnection() {
+        println(dataSource)
+        val test = TestRecord()
+        test.id = "123"
+        test.age = 1
+        test.description = "test"
+        test.name = "steven"
+        testMapper.insert(test)
+    }
 }
